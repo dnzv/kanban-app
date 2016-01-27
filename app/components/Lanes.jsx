95a54f91abd86399ec8f469cb12f9345411933d2
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-01-27 23:54:07 +0200

    Add Lane & Lanes components.

---
+++ app/components/Lanes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Lane from './Lane';
+
+class Lanes extends React.Component {
+  render() {
+    return (
+      <div className="lanes">{this.props.lanes.map(lane =>
+          <Lane className="lane" key={lane.id} lane={lane} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Lanes;
